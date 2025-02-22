9d086851ca53551f50f1b581b5310d70eb386120
boomboom <boomboomturtle@gmail.com>
2025-02-22 18:29:05 +0800

    Deserializing practice

--- hibachi-connect/src/main.rs
+++ hibachi-connect/src/main.rs
@@ -1,25 +1,89 @@
-use std::env;
+use std::{array, env};
 use reqwest::blocking::get;
 use reqwest::Error;
 use std::time::{Duration, Instant};
 use serde_json::Value;
-
 use serde::{Deserialize, Serialize};
-use std::fmt;
 
+#[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize)]
-struct MyObject {
-    level: i32,                    // Assuming level is of type i32
-    lower_threshold: String,       // Lower threshold as a String
-    title: String,                 // Title as a String
-    upper_threshold: String,       // Upper threshold as a String
+struct MarketInventory {
+    crossChainAssets: CrossChainAssets,
+    feeConfig: FeeConfig,
+    markets: Markets,
+    tradingTiers: TradingTiers,
 }
 
+
+// "crossChainAssets": [
+//     {
+//       "chain": "Base",
+//       "exchangeRateFromUSDT": "0.990000",
+//       "exchangeRateToUSDT": "0.990000",
+//       "instantWithdrawalLowerLimitInUSDT": "0.033438705575322546",
+//       "instantWithdrawalUpperLimitInUSDT": "8074.747474747474",
+//       "token": "USDC"
+//     }
+
+#[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize)]
-struct MyData {
-    data: Option <Vec<MyObject>>,  // Optionally contains a vector of MyObject
+struct CrossChainAssets {
+    chain: String,
+    exchangeRateFromUSDT: String,
+    exchangeRateToUSDT: String,
+    instantWithdrawalLowerLimitInUSDT: String,
+    instantWithdrawalUpperLimitInUSDT: String,
+    token: String,
 }
 
+// "feeConfig": {
+//     "depositFees": "0.013209",
+//     "instantWithdrawDstPublicKey": "a4fff986badd3b58ead09cc617a82ff1b5b77b98d560baa27fbcffa4c08610b6372f362f3e8e530291f24251f2c332d958bf776c88ae4370380eee943cddf859",
+//     "instantWithdrawalFees": [
+//       [
+//         1000,
+//         0.01
+//       ]
+//     ],
+//     "tradeMakerFeeRate": "0.00015000",
+//     "tradeTakerFeeRate": "0.00045000",
+//     "transferFeeRate": "0.00010000",
+//     "withdrawalFees": "0.013209"
+//   },
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+struct FeeConfig {
+    depositFees: String,
+    instantWithdrawDstPublicKey: String,
+    instantWithdrawalFees: Vec<(i32, f64)>,
+    tradeMakerFeeRate: String,
+    tradeTakerFeeRate: String,
+    transferFeeRate: String,
+    withdrawalFees: String,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Markets {
+}
+
+// "tradingTiers": [
+//     {
+//       "level": 0,
+//       "lowerThreshold": "0.000000",
+//       "title": "Flicker",
+//       "upperThreshold": "0.000000"
+//     },
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+struct TradingTiers {
+    level: i32,
+    lowerThreshold: String,
+    title: String,
+    upperThreshold: String
+}
 
 const DATA_API_ENDPOINT: &str = "https://data-api.hibachi.xyz/";
 
@@ -45,9 +109,11 @@ fn get_market_inventory() -> Result <(), Error> {
     
     let parsed_value: Value = serde_json::from_str(&response).expect("Failed to parse JSON");
 
-    for key in parsed_value.as_object().unwrap().keys(){
-        println!("{}", key);
-    }
+    println!("{}", parsed_value);
+
+    // for key in parsed_value.as_object().unwrap().keys(){
+    //     println!("{}", key);
+    // }
 
     Ok(())
 }
