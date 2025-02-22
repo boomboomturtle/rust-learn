adaa7f93c2543ba84e78f9c0d9af309ae2b72baa
boomboom <boomboomturtle@gmail.com>
2025-02-22 16:26:39 +0800

    Parsing the value in json

--- hibachi-connect/src/main.rs
+++ hibachi-connect/src/main.rs
@@ -2,14 +2,13 @@ use std::env;
 use reqwest::blocking::get;
 use reqwest::Error;
 use std::time::{Duration, Instant};
+use serde_json::Value;
 
-const GLOBAL_CONSTANT: i32 = 5;
 const DATA_API_ENDPOINT: &str = "https://data-api.hibachi.xyz/";
 
 fn main() {
     let _hibachi_api_key: String = env::var("HIBACHI_API_KEY").expect("HIBACHI_API_KEY not set");
     let _hibachi_private_key: String = env::var("HIBACHI_PRIVATE_KEY").expect("HIBACHI_PRIVATE_KEY not set");
-    println!("The value of the GLOBAL_CONSTANT is = {}", GLOBAL_CONSTANT);
 
     let start_time: Instant = Instant::now();
     let response = get_market_inventory();
@@ -26,7 +25,15 @@ fn get_market_inventory() -> Result <(), Error> {
     // Send a GET request
     let response: String = get(url)?
         .text()?;
-    println!("Response: {}", response);
+    
+    let parsed_value: Value = serde_json::from_str(&response).expect("Failed to parse JSON");
+
+    for key in parsed_value.as_object().unwrap().keys(){
+        println!("{}", key);
+    }
+
+    // println!("parsed_value: {:?}", parsed_value.get("tradingTiers"));
+
     Ok(())
 }
 
