22bdabac760fc8f64f19231a7b27c9fa10aa3888
boomboom <boomboomturtle@gmail.com>
2025-03-01 19:48:52 +0800

    Orderbook and funding rate data

--- websocket-connect/src/main.rs
+++ websocket-connect/src/main.rs
@@ -1,63 +1,30 @@
 use serde_json::json;
+use tokio::net::{TcpListener, TcpStream};
 use tokio_stream::StreamExt;
-use tokio_tungstenite::connect_async;
+use tokio_tungstenite::{connect_async, tungstenite::stream::MaybeTlsStream, WebSocketStream};
 use url::Url;
 use futures_util::{SinkExt};
 
+use std::any::type_name;
+
+fn print_type<T>(_: &T) {
+    println!("Type: {}", type_name::<T>());
+}
+
 const WS_URL: &str = "wss://data-api.hibachi.xyz/ws/market";
 
-#[tokio::main]
-async fn main() {
-    println!("We are going to try to connect to a websocket a listen to Hibachi's messages");
-    let url = Url::parse(WS_URL).expect("Failed to parse a URL");
-
-    match connect_async(url).await {
+async fn orderbook_sub(ob_json_string: String, ob_url: Url) {
+    match connect_async(ob_url).await {
         Ok((mut ws_stream,_)) => {
             println!("Websocket connected");
             
-            let json_message = json!({
-                "method": "subscribe",
-                "parameters": {
-                    "subscriptions": [
-                        {
-                            "symbol": "ETH/USDT-P",
-                            "topic": "mark_price"
-                        },
-                        {
-                            "symbol": "ETH/USDT-P",
-                            "topic": "spot_price"
-                        },
-                        {
-                            "symbol": "ETH/USDT-P",
-                            "topic": "funding_rate_estimation"
-                        },
-                        {
-                            "symbol": "ETH/USDT-P",
-                            "topic": "trades"
-                        },
-                        {
-                            "symbol": "ETH/USDT-P",
-                            "topic": "klines"
-                        },
-                        {
-                            "symbol": "ETH/USDT-P",
-                            "topic": "orderbook"
-                        },
-                        {
-                            "symbol": "ETH/USDT-P",
-                            "topic": "ask_bid_price"
-                        }
-                    ]
-                }
-            });
-
-            let json_string = json_message.to_string();
-
-            match ws_stream.send(tokio_tungstenite::tungstenite::Message::Text(json_string))
+            match ws_stream.send(tokio_tungstenite::tungstenite::Message::Text(ob_json_string))
                 .await {
                     Ok(_) => println!("Message sent"),
                     Err(e) => println!("Message failed = {}", e),
                 }
+
+            print_type(&ws_stream);
                 
             while let Some(Ok(message)) = ws_stream.next().await {
                 println!("Received {}", message);
@@ -68,4 +35,112 @@ async fn main() {
             println!("Connection failed {}", e);
         }
     }
+
 }
+
+async fn funding_rate_sub(ob_json_string: String, ob_url: Url) {
+    match connect_async(ob_url).await {
+        Ok((mut ws_stream,_)) => {
+            println!("Websocket connected");
+            
+            match ws_stream.send(tokio_tungstenite::tungstenite::Message::Text(ob_json_string))
+                .await {
+                    Ok(_) => println!("Message sent"),
+                    Err(e) => println!("Message failed = {}", e),
+                }
+
+            print_type(&ws_stream);
+                
+            while let Some(Ok(message)) = ws_stream.next().await {
+                println!("Received {}", message);
+            };
+
+        }
+        Err(e) => {
+            println!("Connection failed {}", e);
+        }
+    }
+
+
+}
+
+#[tokio::main]
+async fn main() {
+    println!("We are going to try to connect to a websocket a listen to Hibachi's messages");
+    let url = Url::parse(WS_URL).expect("Failed to parse a URL");
+
+    let ob_json_message = json!({
+        "method": "subscribe",
+        "parameters": {
+            "subscriptions": [
+                {
+                    "symbol": "ETH/USDT-P",
+                    "topic": "orderbook"
+                },
+            ]
+        }
+    });
+
+    let ob_json_string = ob_json_message.to_string();
+
+    // orderbook_sub(ob_json_string, url.clone()).await;
+
+    let fr_json_message = json!({
+        "method": "subscribe",
+        "parameters": {
+            "subscriptions": [
+                {
+                    "symbol": "ETH/USDT-P",
+                    "topic": "funding_rate_estimation"
+                },
+            ]
+        }
+    });
+
+    let fr_json_string = fr_json_message.to_string();
+
+    // funding_rate_sub(fr_json_string, url.clone()).await;
+
+    let task_ob = tokio::spawn(orderbook_sub(ob_json_string, url.clone()));
+    let task_fr = tokio::spawn(funding_rate_sub(fr_json_string, url.clone()));
+
+    let _ =tokio::join!(task_ob, task_fr);
+}
+
+
+
+// {
+//     "method": "subscribe",
+//     "parameters": {
+//         "subscriptions": [
+//             {
+//                 "symbol": "ETH/USDT-P",
+//                 "topic": "mark_price"
+//             },
+//             {
+//                 "symbol": "ETH/USDT-P",
+//                 "topic": "spot_price"
+//             },
+//             {
+//                 "symbol": "ETH/USDT-P",
+//                 "topic": "funding_rate_estimation"
+//             },
+//             {
+//                 "symbol": "ETH/USDT-P",
+//                 "topic": "trades"
+//             },
+//             {
+//                 "symbol": "ETH/USDT-P",
+//                 "topic": "klines"
+//             },
+//             {
+//                 "symbol": "ETH/USDT-P",
+//                 "topic": "orderbook"
+//             },
+//             {
+//                 "symbol": "ETH/USDT-P",
+//                 "topic": "ask_bid_price"
+//             }
+//         ]
+//     }
+// }
