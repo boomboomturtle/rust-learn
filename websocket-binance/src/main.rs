cce0bf012893f2f6e1945e8e46be291f1ef4931f
boomboom <boomboomturtle@gmail.com>
2025-03-08 15:04:13 +0800

    Added ticker

--- websocket-binance/src/main.rs
+++ websocket-binance/src/main.rs
@@ -13,11 +13,20 @@ use std::time::{SystemTime, UNIX_EPOCH};
 mod datastructs;
 use datastructs::{special_data_types::*};
 
-const WS_URL: &str = "wss://fstream.binance.com/ws/btcusdt@depth5@100ms";
+const WS_URL: &str = "wss://fstream.binance.com/";
+// ws/btcusdt@depth5@100ms";
+// <symbol>@bookTicker
 
 
-async fn orderbook_sub(ob_url: Url) {
-    match connect_async(ob_url.to_string()).await {
+
+async fn orderbook_sub() {
+    let mut url: String = WS_URL.to_owned();
+    let url_appendage: &str = "ws/btcusdt@depth5@0ms";
+    url.push_str(url_appendage);
+
+    let ws_url = Url::parse(&url).expect("Failed to parse a URL");
+
+    match connect_async(ws_url.to_string()).await {
         Ok((mut ws_stream,_)) => {
             println!("Websocket connected");
             
@@ -27,7 +36,7 @@ async fn orderbook_sub(ob_url: Url) {
                 
                 println!("Received {:?} {}", since_epoch.as_millis(), message);
 
-                let _parsed_struct: BinanceOrderbook = serde_json::from_str(&message.to_string()).expect("Failed to parse JSON");
+                // let _parsed_struct: BinanceOrderbook = serde_json::from_str(&message.to_string()).expect("Failed to parse JSON");
                 }
             }
 
@@ -37,13 +46,39 @@ async fn orderbook_sub(ob_url: Url) {
     }
 }
 
+async fn orderbook_ticker() {
+    let mut url: String = WS_URL.to_owned();
+    let url_appendage: &str = "ws/btcusdt@bookTicker";
+    url.push_str(url_appendage);
+
+    let ws_url = Url::parse(&url).expect("Failed to parse a URL");
+
+    match connect_async(ws_url.to_string()).await {
+        Ok((mut ws_stream,_)) => {
+            println!("Websocket connected");
+            
+            while let Some(Ok(message)) = ws_stream.next().await {
+                let start = SystemTime::now();
+                let since_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+                
+                println!("Received {:?} {}", since_epoch.as_millis(), message);
+
+                // let _parsed_struct: BinanceOrderbook = serde_json::from_str(&message.to_string()).expect("Failed to parse JSON");
+                }
+            }
+
+        Err(e) => {
+            println!("Connection failed {}", e);
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     println!("We are going to try to connect to a websocket a listen to Binance's messages");
-    let url = Url::parse(WS_URL).expect("Failed to parse a URL");
 
-    let task_ob = tokio::spawn(orderbook_sub(url.clone()));
+    let task_ob = tokio::spawn(orderbook_sub());
+    let task_ticker = tokio::spawn(orderbook_ticker());
 
     let _ =tokio::join!(task_ob);
 }
